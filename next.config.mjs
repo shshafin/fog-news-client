57020914f987103e6d0c43c344104a94f689f437
Sha Fin <99030043+shshafin@users.noreply.github.com>
2025-10-09 10:21:33 +0600

    Add image domain and API rewrite configuration to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,10 +7,22 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ["api2.tiresdash.com", "images.pexels.com"], // external domains
+    domains: [
+      "api2.tiresdash.com",
+      "newsportal.marcelinestudios.com:5001",
+      "images.pexels.com",
+    ], // external domains
     deviceSizes: [320, 420, 768, 1024, 1200], // responsive sizes
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384], // small icons
     unoptimized: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // frontend e je route call korbe
+        destination: "http://newsportal.marcelinestudios.com:5001/api/:path*", // backend HTTP
+      },
+    ];
   },
 };
 
